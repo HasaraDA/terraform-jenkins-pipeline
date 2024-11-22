b1a4c0e1d0891a7b15f025fa667d0b5f5587ef39
Hasara Dulanjana <hasara.dulanjana.16@gmail.com>
2024-11-22 19:19:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name  # Reference to your key pair
+  
+  # VPC and Subnet configuration
+  subnet_id                 = var.subnet_id
+  vpc_security_group_ids    = [var.security_group_id]
 
- tags = {
-   Name = var.name_tag,
- }
+  tags = {
+    Name = var.name_tag
+  }
 }

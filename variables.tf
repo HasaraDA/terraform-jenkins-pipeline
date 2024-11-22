41082118279d8c47cf34dde91ed706cf3859225f
Hasara Dulanjana <hasara.dulanjana.16@gmail.com>
2024-11-22 19:20:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,21 +16,39 @@ variable "aws_region" {
   default     = "ap-southeast-1"
 }
 
-
 variable "ami" {
-   type        = string
-   description = "Ubuntu AMI ID"
-   default     = "ami-047126e50991d067b"
+  type        = string
+  description = "Ubuntu AMI ID"
+  default     = "ami-047126e50991d067b"
 }
 
 variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.micro"
+  type        = string
+  description = "Instance type"
+  default     = "t2.micro"
 }
 
 variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
+  type        = string
+  description = "Name of the EC2 instance"
+  default     = "My EC2 Instance"
+}
+
+# VPC and Subnet details
+variable "subnet_id" {
+  description = "The Subnet ID to launch the EC2 instance in"
+  type        = string
+  default     = "subnet-020cb3fa62e38cb07"
+}
+
+variable "security_group_id" {
+  description = "The Security Group ID to associate with the EC2 instance"
+  type        = string
+  default     = ""  # If you have a specific security group, provide the ID here
+}
+
+variable "key_name" {
+  description = "The key pair name to use for EC2 instance"
+  type        = string
+  default     = "MyKeyPair"
 }
